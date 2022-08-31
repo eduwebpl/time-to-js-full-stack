ad436007558f15afaecd20cd25226dee8acd5fe0
Jabi <michal.jabi@gmail.com>
2022-08-31 11:20:57 +0200

    dev(http): additional day-to-day work tools (linter + prettier)

--- back-end-http/src/server.js
+++ back-end-http/src/server.js
@@ -3,17 +3,16 @@ import http from 'http'
 const { PORT = 3000 } = process.env
 
 const server = http.createServer((req, res) => {
+  console.log(req.url)
+  console.log(req.method)
+  console.log(req.headers)
 
-	console.log(req.url)
-	console.log(req.method)
-	console.log(req.headers)
-
-	res.writeHead(200, {'Content-Type': 'application/json'})
-	res.write(JSON.stringify({ hello: 'World' }))
-	res.end()
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.write(JSON.stringify({ hello: 'World' }))
+  res.end()
 })
 
 server.listen(PORT)
 server.on('listening', () => {
-	console.log(`Server is listening on http://localhost:${PORT}/`)
+  console.log(`Server is listening on http://localhost:${PORT}/`)
 })
